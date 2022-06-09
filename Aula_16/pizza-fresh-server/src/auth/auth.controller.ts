6830a2f3249fb225d6fb3446f8c2ade6442a01c6
Leo Ruiz <ruizruizleo@hotmail.com>
2022-06-08 22:18:06 -0300

    final

--- Aula_16/pizza-fresh-server/src/auth/auth.controller.ts
+++ Aula_16/pizza-fresh-server/src/auth/auth.controller.ts
@@ -1,9 +1,11 @@
 import { Post, Controller, Body, HttpCode, HttpStatus, Get, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { User } from 'src/user/entities/user.entity';
 import { AuthService } from './auth.service';
 import { LoginResponseDto } from './dto/login-response.dto';
 import { LoginDto } from './dto/login.dto';
+import { LoggedUser } from './logged-user.decorator';
 
 @Controller('auth')
 @ApiTags('auth')
@@ -22,7 +24,7 @@ export class AuthController {
   @Get()
   @UseGuards(AuthGuard())
   @ApiBearerAuth()
-  profile() {
-    return { message: 'Autenticação bem sucedida' };
+  profile(@LoggedUser() user: User) {
+    return user;
   }
 }
